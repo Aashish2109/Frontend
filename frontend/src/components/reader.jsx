9235890d1b87e588a362c181b7d156039db1ac29
Aashish Gupta <aashishg863@gmail.com>
2024-04-29 19:01:46 +0530

    added

---
+++ frontend/src/components/reader.jsx
@@ -0,0 +1,114 @@
+import { useState } from "react";
+import styles from "./admin.module.scss";
+import Login from "../components/images/login.jpg";
+import { Link, useNavigate } from "react-router-dom";
+const Reader = () => {
+  const [email, setEmail] = useState("");
+  const [otp, setOtp] = useState("");
+const navigate=useNavigate()
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("/send-otp", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email }),
+      });
+      const data = await response.json();
+
+      if (data.error) {
+        alert(data.error);
+      } else {
+        if (data.owner_role === "Admin") {
+          alert("OTP Sent Successfully");
+        } else {
+          alert("Reader does not exist");
+        }
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const handleOtpSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(`/validate-otp/${otp}`, {
+        method: "POST",
+      });
+      const data = await response.json();
+
+      if (data.error) {
+        alert(data.error);
+      } else {
+        alert("Reader Login Successfully");
+        navigate('/readerfunctions')
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <>
+      <div className={styles.setMain}>
+        <div className={styles.mainContainer}>
+          <h1>Welcome Back</h1>
+          <h3>Hi Reader, Please Login To Continue</h3>
+          <div id={styles.loginContainer}>
+            <form onSubmit={handleLogin}>
+              <label htmlFor="email">Enter Email:</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required pattern="\S+.*"
+                autoComplete="off"
+                placeholder="Please enter your email"
+              />
+              <br />
+              <button type="submit" className={styles.buttonClass}>
+                Login
+              </button>
+            </form>
+            <div className={styles.createaccount}>
+              <Link to="/index"><button className={styles.buttonClass}>Previous</button></Link>
+                
+              <Link to="/readercreateaccount">
+                <button className={styles.buttonClass}>Create Account</button>
+              </Link>
+            </div>
+          </div>
+          <div id={styles.otpContainer}>
+            <h2>Enter OTP</h2>
+            <form onSubmit={handleOtpSubmit}>
+              <label htmlFor="otp">OTP:</label>
+              <input
+                type="text"
+                id="otp"
+                name="otp"
+                value={otp}
+                onChange={(e) => setOtp(e.target.value)}
+                required pattern="\S+.*"
+              />
+              <button className={styles.buttonClass} type="submit">
+                Submit OTP
+              </button>
+            </form>
+          </div>
+        </div>
+        <div>
+          <img src={Login} alt="login" className={styles.imageclass} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Reader;
---
+++ frontend/src/components/readercreateaccount.jsx
@@ -0,0 +1,116 @@
+import React, { useState } from "react";
+import styles from "./readercreateaccount.module.scss";
+import SignUpImage from "../components/images/signup.jpg";
+import { Link } from "react-router-dom";
+const ReaderCreateAccount = () => {
+  const [libraryName, setLibraryName] = useState("");
+  const [readerName, setReaderName] = useState("");
+  const [readerEmail, setReaderEmail] = useState("");
+  const [readerPhone, setReaderPhone] = useState("");
+
+  const Handler = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("/createaccount", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          library_name: libraryName,
+          owner_name: readerName,
+          owner_email: readerEmail,
+          owner_phone: readerPhone,
+          owner_role: "Reader",
+        }),
+      });
+      const data = await response.json();
+     if(response.status===404){
+      alert("library not found")
+     } else {
+        alert("reader created successfully");
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+  return (
+    <>
+      <div className={styles.readerCreate}>
+        <div>
+          <img className={styles.SignUpImage} src={SignUpImage} alt="signupimage" />
+        </div>
+
+        <div className={styles.readerContainer}>
+          <h1>Create A New Account</h1>
+          <form onSubmit={Handler}>
+            <label htmlFor="libraryName">Library Name:</label>
+            <input
+              className={styles.inputField}
+              type="text"
+              id="libraryName"
+              name="library_name"
+              placeholder="Enter Library Name"
+              required pattern="\S+.*"
+              autoComplete="off"
+              value={libraryName}
+              onChange={(e) => setLibraryName(e.target.value)}
+            />
+            <br />
+            <label htmlFor="OwnerName">Reader Name:</label>
+            <input
+              className={styles.inputField}
+              type="text"
+              id="OwnerName"
+              name="owner_name"
+              placeholder="Enter Reader Name"
+              required pattern="\S+.*"
+              autoComplete="off"
+              value={readerName}
+              onChange={(e) => setReaderName(e.target.value)}
+            />
+            <br />
+            <label htmlFor="OwnerEmail">Reader Email:</label>
+            <input
+              className={styles.inputField}
+              type="email"
+              id="OwnerEmail"
+              name="owner_email"
+              placeholder="Enter Reader Email"
+              required pattern="\S+.*"
+              autoComplete="off"
+              value={readerEmail}
+              onChange={(e) => setReaderEmail(e.target.value)}
+            />
+            <br />
+            <label htmlFor="OwnerPhone">Reader Phone:</label>
+            <input
+              className={styles.inputField}
+              type="tel"
+              id="OwnerPhone"
+              name="owner_phone"
+              placeholder="Enter Contact Number"
+              required pattern="[1-9]{1}[0-9]{9}"
+              autoComplete="off"
+              value={readerPhone}
+              onChange={(e) => setReaderPhone(e.target.value)}
+            />
+            <br />
+            <label htmlFor="OwnerRole">Role:</label>
+            <input className={styles.inputField} type="text" value="Reader" />
+            <br />
+            <button class={styles.buttonClass} type="submit">
+              Create Account
+            </button>
+          </form>
+
+          <Link to="/reader">
+            <button className={styles.buttonClass}>Login</button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ReaderCreateAccount;
